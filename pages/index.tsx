13c0e0ac8af4b7042fcb1fa13ae1b4f09a13312d
Peter Parker <web3slinger@gmail.com>
2022-03-27 21:03:42 +0800

    Add navbar and styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 
+import Loader from '../components/Loader'
+
 const Home: NextPage = () => {
   return (
     <div>
-      <Link
-        prefetch={false}
-        href={{
-          pathname: '/[username]',
-          query: { username: 'Peter' },
-        }}
-      >
-        <a>Peter&apos;s Profile</a>
-      </Link>
+      <Loader show />
     </div>
   )
 }
