9efadb0b015010979aa777a7d0987e4189540d40
Peter Parker <web3slinger@gmail.com>
2022-03-31 12:13:26 +0800

    Implement like function

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,8 +1,14 @@
 import styles from '../../styles/Post.module.css'
 import PostContent from '../../components/PostContent'
-import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase'
-import { useDocumentData } from 'react-firebase-hooks/firestore'
+import HeartButton from '../../components/HeartButton'
+import AuthCheck from '../../components/AuthCheck'
 import Metatags from '../../components/Metatags'
+import { UserContext } from '../../lib/context'
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase'
+
+import Link from 'next/link'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+import { useContext } from 'react'
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params
@@ -20,7 +26,7 @@ export async function getStaticProps({ params }) {
 
   return {
     props: { post, path },
-    revalidate: 5000,
+    revalidate: 100,
   }
 }
 
@@ -44,15 +50,19 @@ export async function getStaticPaths() {
     fallback: 'blocking',
   }
 }
+
 export default function Post(props) {
   const postRef = firestore.doc(props.path)
   const [realtimePost] = useDocumentData(postRef)
 
   const post = realtimePost || props.post
 
+  const { user: currentUser } = useContext(UserContext)
+
   return (
     <main className={styles.container}>
-      <Metatags />
+      <Metatags title={post.title} description={post.title} />
+
       <section>
         <PostContent post={post} />
       </section>
@@ -61,6 +71,22 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
+
+        <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
+
+        {currentUser?.uid === post.uid && (
+          <Link href={`/admin/${post.slug}`}>
+            <button className="btn-blue">Edit Post</button>
+          </Link>
+        )}
       </aside>
     </main>
   )
